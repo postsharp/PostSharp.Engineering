6f0b6717aa505bd058dda298ce34dfdf40849357
Gael Fraiteur <gael@postsharp.net>
2021-12-03 15:45:43 +0100

    Fixed Kotlin script.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,28 +2,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2019.2"
 
 project {
@@ -51,7 +29,7 @@ object DebugBuild : BuildType({
                 path = "Build.ps1"
             }
             noProfile = false
-            param("jetbrains_powershell_scriptArguments", "build --numbered %build.number%")
+            param("jetbrains_powershell_scriptArguments", "test --numbered %build.number%")
         }
     }
 
@@ -81,7 +59,7 @@ object ReleaseBuild : BuildType({
                 path = "Build.ps1"
             }
             noProfile = false
-            param("jetbrains_powershell_scriptArguments", "build --public --configuration Release --sign")
+            param("jetbrains_powershell_scriptArguments", "test --public --configuration Release --sign")
         }
     }
 
