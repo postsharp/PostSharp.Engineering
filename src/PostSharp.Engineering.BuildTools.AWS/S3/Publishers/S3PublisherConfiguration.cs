7f23d73a1736e556f8c4419a81d6c2a5b64ec472
Jan Hlavac <jan@postsharp.net>
2022-02-24 16:44:47 +0100

    #29757 Added AWS S3 Publisher

---
+++ src/PostSharp.Engineering.BuildTools.AWS/S3/Publishers/S3PublisherConfiguration.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) SharpCrafters s.r.o.All rights reserved.Released under the MIT license.
+
+using Amazon;
+using PostSharp.Engineering.BuildTools.Build.Model;
+
+namespace PostSharp.Engineering.BuildTools.AWS.S3.Publishers
+{
+    public class S3PublisherConfiguration
+    {
+        public ParametricString PackageFileName { get; init; }
+
+        public RegionEndpoint RegionEnpoint { get; init; }
+
+        public string BucketName { get; init; }
+
+        public string KeyName { get; init; }
+
+        public S3PublisherConfiguration(
+            ParametricString packageFileName,
+            RegionEndpoint regionEnpoint,
+            string bucketName,
+            string keyName )
+        {
+            this.PackageFileName = packageFileName;
+            this.RegionEnpoint = regionEnpoint;
+            this.BucketName = bucketName;
+            this.KeyName = keyName;
+        }
+    }
+}
