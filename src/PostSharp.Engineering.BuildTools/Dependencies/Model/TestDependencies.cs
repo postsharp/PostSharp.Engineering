26e9b2f8f21d15821bc6d82d3edf8f90f9e5baaa
Jan Hlavac <jan@postsharp.net>
2022-03-23 17:41:38 +0100

    Added TestDependencies

---
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Model/TestDependencies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+namespace PostSharp.Engineering.BuildTools.Dependencies.Model;
+
+public static class TestDependencies
+{
+    public static DependencyDefinition Product { get; } = new(
+        "PostSharp.Engineering.Test.TestProduct",
+        VcsProvider.AzureRepos,
+        "postsharp",
+        ("Test_PostSharpEngineeringTestProduct_DebugBuild", "Test_PostSharpEngineeringTestProduct_ReleaseBuild", "Test_PostSharpEngineeringTestProduct_PublicBuild") );
+    
+    public static DependencyDefinition Dependency { get; } = new(
+        "PostSharp.Engineering.Test.Dependency",
+        VcsProvider.AzureRepos,
+        "postsharp",
+        ("Test_PostSharpEngineeringTestDependency_DebugBuild", "Test_PostSharpEngineeringTestDependency_ReleaseBuild", "Test_PostSharpEngineeringTestDependency_PublicBuild") );
+    
+    public static DependencyDefinition TransitiveDependency { get; } = new(
+        "PostSharp.Engineering.Test.TransitiveDependency",
+        VcsProvider.AzureRepos,
+        "postsharp",
+        ("Test_PostSharpEngineeringTestTransitiveDependency_DebugBuild", "Test_PostSharpEngineeringTestTransitiveDependency_ReleaseBuild", "Test_PostSharpEngineeringTestTransitiveDependency_PublicBuild") );
+
+    public static ImmutableArray<DependencyDefinition> All { get; } = ImmutableArray.Create(
+        Product,
+        Dependency,
+        TransitiveDependency );
+}
