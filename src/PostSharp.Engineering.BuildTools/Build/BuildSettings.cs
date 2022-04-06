6e99c7b45fc8e9a5347cde48a7b8c2c6b189e131
Gael Fraiteur <gael@postsharp.net>
2022-04-06 11:17:36 +0200

    Code review.

--- src/PostSharp.Engineering.BuildTools/Build/BuildSettings.cs
+++ src/PostSharp.Engineering.BuildTools/Build/BuildSettings.cs
@@ -30,10 +30,6 @@ namespace PostSharp.Engineering.BuildTools.Build
         [Description( "Disables concurrent processing" )]
         [CommandOption( "--no-concurrency" )]
         public bool NoConcurrency { get; set; }
-        
-        [Description("Simulate a continuous integration build by setting the build ContinuousIntegrationBuild properyt to TRUE.")]
-        [CommandOption("--ci")]
-        public bool ContinuousIntegration { get; set; }
 
         public BuildSettings WithIncludeTests( bool value )
         {
@@ -63,9 +59,9 @@ namespace PostSharp.Engineering.BuildTools.Build
 
             return clone;
         }
-        
+
         public VersionSpec GetVersionSpec( BuildConfiguration configuration )
-            => configuration == Build.BuildConfiguration.Public
+            => configuration == BuildConfiguration.Public
                 ? new VersionSpec( VersionKind.Public )
                 : this.BuildNumber > 0
                     ? new VersionSpec( VersionKind.Numbered, this.BuildNumber )
