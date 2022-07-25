2a4fcfd2f1118710e7b91f5c719c1b2d1632555e
Jan Hlavac <jan@postsharp.net>
2022-07-25 17:23:18 +0200

    #30777 Split VcsRepo to separate file.

--- src/PostSharp.Engineering.BuildTools/Dependencies/Model/DependencyDefinition.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Model/DependencyDefinition.cs
@@ -2,27 +2,6 @@
 
 namespace PostSharp.Engineering.BuildTools.Dependencies.Model
 {
-    public class VcsRepo
-    {
-        
-        public string RepoName { get; }
-        
-        public string ProjectName { get; }
-
-        public VcsProvider Provider { get; }
-        
-        public string RepoUrl => this.Provider.GetRepoUrl( this );
-
-        public string DownloadTextFile( string branch, string path ) => this.Provider.DownloadTextFile( this, branch, path );
-
-        public VcsRepo( string repoName, string projectName, VcsProvider provider )
-        {
-            this.RepoName = repoName;
-            this.ProjectName = projectName;
-            this.Provider = provider;
-        }
-    }
-    
     public class DependencyDefinition
     {
         public string Name { get; }
