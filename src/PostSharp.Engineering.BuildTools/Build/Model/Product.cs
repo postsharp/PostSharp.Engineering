c3d048f60edeb9980acb57b210a27de4f1ce2f0e
GaelFraiteur <gael@postsharp.net>
2023-03-31 11:11:08 +0200

    Search indexer

--- src/PostSharp.Engineering.BuildTools/Build/Model/Product.cs
+++ src/PostSharp.Engineering.BuildTools/Build/Model/Product.cs
@@ -166,6 +166,8 @@ namespace PostSharp.Engineering.BuildTools.Build.Model
 
         public ImmutableArray<DotNetTool> DotNetTools { get; init; } = DotNetTool.DefaultTools;
 
+        public ProductExtension[] Extensions { get; init; } = Array.Empty<ProductExtension>();
+
         public bool Build( BuildContext context, BuildSettings settings )
         {
             var configuration = settings.BuildConfiguration;
@@ -1927,6 +1929,15 @@ namespace PostSharp.Engineering.BuildTools.Build.Model
                             name: $"Version Bump",
                             buildArguments: $"bump",
                             buildAgentType: this.BuildAgentType ) { IsDeployment = true } );
+                }
+            }
+            
+            // Add from extensions.
+            foreach ( var extension in this.Extensions )
+            {
+                if ( !extension.AddTeamcityBuildConfiguration( context, teamCityBuildConfigurations ) )
+                {
+                    return false;
                 }
             }
 
