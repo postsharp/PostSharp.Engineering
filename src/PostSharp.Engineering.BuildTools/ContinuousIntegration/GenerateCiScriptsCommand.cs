cdfec800f1681e425add4d5d886c94e1a768984c
Antonín Procházka <antonin@postsharp.net>
2023-08-16 10:24:42 +0200

    #33615 GenerateCiScriptsCommand moved to ContinuousIntegration namespace.

--- src/PostSharp.Engineering.BuildTools/ContinuousIntegration/GenerateCiScriptsCommand.cs
+++ src/PostSharp.Engineering.BuildTools/ContinuousIntegration/GenerateCiScriptsCommand.cs
@@ -1,6 +1,8 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
-namespace PostSharp.Engineering.BuildTools.Build;
+using PostSharp.Engineering.BuildTools.Build;
+
+namespace PostSharp.Engineering.BuildTools.ContinuousIntegration;
 
 public class GenerateCiScriptsCommand : BaseCommand<CommonCommandSettings>
 {
