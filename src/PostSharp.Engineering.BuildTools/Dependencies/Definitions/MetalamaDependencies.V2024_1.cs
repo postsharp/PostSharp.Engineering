97887776b8d30171e6578aa49932bef571997d6d
Antonín Procházka <antonin@postsharp.net>
2024-01-08 08:59:20 +0100

    #34256 PostSharp added as Metalama 2024.1 dependency.

--- src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/MetalamaDependencies.V2024_1.cs
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/MetalamaDependencies.V2024_1.cs
@@ -43,7 +43,7 @@ public static partial class MetalamaDependencies
                     isVersioned ) { }
         }
 
-        public static ProductFamily Family { get; } = new( _projectName, "2024.1", DevelopmentDependencies.Family )
+        public static ProductFamily Family { get; } = new( _projectName, "2024.1", DevelopmentDependencies.Family, PostSharpDependencies.V2024_1.Family )
         {
             UpstreamProductFamily = V2024_0.Family
 
