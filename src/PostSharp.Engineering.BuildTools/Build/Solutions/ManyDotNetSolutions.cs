51569ed395c2da8f8a513b3bb52a344247323752
GaelFraiteur <gael@postsharp.net>
2024-05-15 11:42:35 +0200

    ManyDotNetSolutions improvements.

--- src/PostSharp.Engineering.BuildTools/Build/Solutions/ManyDotNetSolutions.cs
+++ src/PostSharp.Engineering.BuildTools/Build/Solutions/ManyDotNetSolutions.cs
@@ -22,20 +22,12 @@ public class ManyDotNetSolutions : Solution
     /// <param name="directory">A directory.</param>
     public ManyDotNetSolutions( string directory ) : base( directory )
     {
+        // Default settings.
         this.IsTestOnly = true;
+        this.BuildMethod = Model.BuildMethod.Build;
     }
-
-    public override bool Build( BuildContext context, BuildSettings settings )
-    {
-        throw new NotSupportedException();
-    }
-
-    public override bool Pack( BuildContext context, BuildSettings settings )
-    {
-        throw new NotSupportedException();
-    }
-
-    public override bool Test( BuildContext context, BuildSettings settings )
+    
+    private bool BuildOrTest( BuildContext context, BuildSettings settings, bool test, string verb )
     {
         var failedProjects = new List<DotNetSolution>();
 
@@ -66,7 +58,7 @@ public class ManyDotNetSolutions : Solution
                     {
                         if ( solution.Build( localContext, settings ) )
                         {
-                            if ( solution.TestMethod == Model.BuildMethod.Test )
+                            if ( test && solution.TestMethod == Model.BuildMethod.Test )
                             {
                                 if ( !solution.Test( localContext, settings ) )
                                 {
@@ -86,7 +78,7 @@ public class ManyDotNetSolutions : Solution
                         // Write the output, but within a lock to avoid mixes.
                         lock ( consoleSync )
                         {
-                            context.Console.WriteHeading( $"Testing {solution.SolutionPath}" );
+                            context.Console.WriteHeading( $"{verb} {solution.SolutionPath}" );
                             bufferingConsole.Replay();
                         }
                     }
@@ -106,6 +98,17 @@ public class ManyDotNetSolutions : Solution
 
         return true;
     }
+
+    public override bool Build( BuildContext context, BuildSettings settings )
+        => this.BuildOrTest( context, settings, false, "Building" );
+
+    public override bool Pack( BuildContext context, BuildSettings settings )
+    {
+        throw new NotSupportedException();
+    }
+
+    public override bool Test( BuildContext context, BuildSettings settings ) 
+        => this.BuildOrTest( context, settings, true, "Testing" );
 
     public override bool Restore( BuildContext context, BuildSettings settings )
     {
