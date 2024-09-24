f16c62deb5f93d9bcdf3a74eee93c141b66b1ac5
Antonín Procházka <antonin@postsharp.net>
2024-09-24 11:46:22 +0200

    PostSharp 2025.0 introduced.

---
+++ src/PostSharp.Engineering.BuildTools/Dependencies/Definitions/PostSharpDependencies.V2025_0.cs
@@ -0,0 +1,49 @@
+// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using JetBrains.Annotations;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration;
+using PostSharp.Engineering.BuildTools.ContinuousIntegration.Model;
+using PostSharp.Engineering.BuildTools.Dependencies.Model;
+
+namespace PostSharp.Engineering.BuildTools.Dependencies.Definitions;
+
+public static partial class PostSharpDependencies
+{
+    // ReSharper disable once InconsistentNaming
+
+    [PublicAPI]
+    public static class V2025_0
+    {
+        private class PostSharpDependencyDefinition : DependencyDefinition
+        {
+            private static readonly TeamCityProjectId _teamCityProjectId = new(
+                $"{_projectName}_{_projectName}{Family.VersionWithoutDots}",
+                _projectName );
+
+            private static readonly string _distributionBuildId = $"{_teamCityProjectId}_BuildDistribution";
+
+            public PostSharpDependencyDefinition()
+                : base(
+                    Family,
+                    "PostSharpPackage",
+                    $"release/{Family.Version}",
+                    null,
+                    new AzureDevOpsRepository( _projectName, _projectName ),
+                    new CiProjectConfiguration(
+                        _teamCityProjectId,
+                        new ConfigurationSpecific<string>( "not-used", _distributionBuildId, "not-used" ),
+                        null,
+                        null,
+                        TeamCityHelper.TeamCityCloudTokenEnvironmentVariableName,
+                        TeamCityHelper.TeamCityCloudUrl ),
+                    false )
+            {
+                this.EngineeringDirectory = @"PrivateBuild\Distribution\eng";
+            }
+        }
+
+        public static ProductFamily Family { get; } = new( _projectName, "2025.0", DevelopmentDependencies.Family );
+
+        public static DependencyDefinition PostSharp { get; } = new PostSharpDependencyDefinition();
+    }
+}
